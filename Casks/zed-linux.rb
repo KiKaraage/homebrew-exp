62251a85361837501e0a23ee05bd9f35110190c2
Ira Limitanei <47409392+lambdaclan@users.noreply.github.com>
2026-08-25 03:56:22 +0000

    zed-linux 1.16.2

--- Casks/zed-linux.rb
+++ Casks/zed-linux.rb
@@ -1,6 +1,6 @@
 cask "zed-linux" do
-  version "1.14.2"
-  sha256 "6c476103b49a87dc62c46b8cc4dca84c0458ed53537de664ebdbc99b47daee2d"
+  version "1.16.2"
+  sha256 "c761acb9e52977924c6a36a10224cfb72985e6bef5c1e9e2c6f24e9748b106a7"
 
   url "https://github.com/zed-industries/zed/releases/download/v#{version}/zed-linux-x86_64.tar.gz"
   name "Zed"
