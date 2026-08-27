7fbe186e3a741c5e747e3568ca4b48d58661300f
Ira Limitanei <47409392+lambdaclan@users.noreply.github.com>
2026-08-27 13:57:27 +0000

    craft-agents-linux 0.12.1

--- Casks/craft-agents-linux.rb
+++ Casks/craft-agents-linux.rb
@@ -1,6 +1,6 @@
 cask "craft-agents-linux" do
-  version "0.11.2"
-  sha256 "a0d299c27a88f777a55761b847de3564373bacd067b7433642809bda64e1c677"
+  version "0.12.1"
+  sha256 "8a106012935f638b3af25303fc9fbf517e05f8b5726550dc21683308b253e47d"
 
   url "https://github.com/craft-ai-agents/craft-agents-oss/releases/download/v#{version}/Craft-Agents-#{version}-linux-x64.AppImage"
   name "Craft Agents"
