c01f52ac592c2dbed545aa644e10d9737ff867cc
Ira Limitanei <47409392+lambdaclan@users.noreply.github.com>
2026-09-02 07:44:30 +0000

    claude-desktop-linux 1.40609.1

--- Casks/claude-desktop-linux.rb
+++ Casks/claude-desktop-linux.rb
@@ -1,6 +1,6 @@
 cask "claude-desktop-linux" do
-  version "1.22209.3"
-  sha256 "d427f46ac9233dbc4d8a441a602f09f750b8a5f05d1fc7a00285d7a6ce07655c"
+  version "1.40609.1"
+  sha256 "80182e8511c6bbee6de26c7ee225fbd2a9aba2274ef1405a1d89cd8fe7a380dc"
 
   url "https://downloads.claude.ai/claude-desktop/apt/stable/pool/main/c/claude-desktop/claude-desktop_#{version}_amd64.deb"
   name "Claude"
